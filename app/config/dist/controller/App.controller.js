f71a1f4203338add4b8d3c09e7ff058e2ba1fc5a
Deep Goyal <goyaldeepanshu2906@gmail.com>
2024-10-24 09:46:28 +0000

    deployment configuration backend done successfully with dummy auth

---
+++ app/config/dist/controller/App.controller.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"],function(n){"use strict";return n.extend("config.controller.App",{onInit:function(){},onPressBack:function(){window.history.back()}})});
+//# sourceMappingURL=App.controller.js.map
---
+++ app/inventory/dist/controller/App.controller.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"],function(n){"use strict";return n.extend("inventory.controller.App",{onInit:function(){},onPressBack:function(){window.history.back()}})});
+//# sourceMappingURL=App.controller.js.map
